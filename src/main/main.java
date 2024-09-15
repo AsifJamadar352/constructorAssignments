2c73087bbb6f94f86b954ebcebe4abfb26edfd14
AsifJamadar352 <asifjamadar352@gmail.com>
2024-09-15 23:59:39 +0530

    first commit

---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+public class main{
+
+
+    public static void main(String[] args){
+        Person p = new Person("Asif",28,"M");
+        system.out.println(p.getName() +" :"+p1.getage()+" :"+p.getgender());
+
+        Person p1=new Person("khalid",5,"NA");
+        system.out.println(p1.getName()+" :"+p1.getage+" :"+p1.getgender());
+
+        Person p2=new Person("saad");
+        system.out.println(p2.getName());
+        p2.setage(28);
+        system.out.println(p2.getName()+" :"+p1.getage));
+    }
+}
